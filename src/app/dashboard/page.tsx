aa91b2148ce63d9bb6cb240b0642c0484ea6ca6d
skyZoneHub <myselfownersky007@gmail.com>
2025-05-05 22:33:53 +0530

    dashboard authOtions fixed

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 // src/app/dashboard/page.tsx
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../api/auth/[...nextauth]/route';
+import { authOptions } from '@/lib/auth';
 import { redirect } from 'next/navigation';
 import Image from 'next/image';
 
